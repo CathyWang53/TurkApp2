e6bb0a77b9d7706c7c42c15f61bf81cad19d5436
Cathy Wang <cathy@er-10-131-182-216.wireless.umn.edu>
2018-09-13 17:54:45 -0500

    Add Heroku related config

--- website1/__init__.py
+++ website1/__init__.py
@@ -1,25 +1,25 @@
-#import os
+import os
 from flask import Flask, redirect
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-#migrate = Migrate()
-#db = SQLAlchemy()
+migrate = Migrate()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     
-#    app.config.from_mapping(
-#                            SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-#                            SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-#                            'sqlite:///' + os.path.join(app.instance_path, 'website1.sqlite'),
-#                            SQLALCHEMY_TRACK_MODIFICATIONS = False
-#                            )
-#
-#    db.init_app(app)
-#    migrate.init_app(app, db)
-    
-#    from . import models
+    app.config.from_mapping(
+                            SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
+                            SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                            'sqlite:///' + os.path.join(app.instance_path, 'website1.sqlite'),
+                            SQLALCHEMY_TRACK_MODIFICATIONS = False
+                            )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import models
 
     @app.route('/test')
     def test():
